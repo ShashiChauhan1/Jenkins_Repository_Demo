b6a971fe146cf3a7ce8d18995241c504cf6e76f6
ShashiChauhan1 <107924881+ShashiChauhan1@users.noreply.github.com>
2022-07-18 12:45:23 +0100

    Tkt 12.42

---
+++ src/test/java/axone/git/TestingForJenkins.java
@@ -0,0 +1,12 @@
+package axone.git;
+
+import org.testng.annotations.Test;
+
+public class TestingForJenkins {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Testing for jenkins");
+    }
+}
