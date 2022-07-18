a09657019d649d939737b93c971766e3e2c39bfb
ShashiChauhan1 <107924881+ShashiChauhan1@users.noreply.github.com>
2022-07-18 11:02:40 +0100

    Tkt 11.02

---
+++ src/test/java/axone/git/TestingGit_Repo.java
@@ -0,0 +1,13 @@
+package axone.git;
+
+import org.testng.annotations.Test;
+
+public class TestingGit_Repo {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("testing 1");
+    }
+
+}
